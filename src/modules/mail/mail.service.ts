cff805e2a0044d51aeacc91443ab34596fb024eb
Lucas F. Leandro <lucasfariasleandro@gmail.com>
2026-03-06 11:49:39 -0300

    refactor: email verify

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -5,7 +5,9 @@ import * as nodemailer from "nodemailer";
 export class MailService {
 
   private transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
