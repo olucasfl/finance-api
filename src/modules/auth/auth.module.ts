69d99d57e71600f5ce9249de8482f5a049023446
Lucas Leandro <lucasfariasleandro@gmail.com>
2026-03-06 15:07:21 -0300

    fix: add MailService to Auth Module imports

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,14 +4,16 @@ import { AuthService } from './auth.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
   imports: [
     PrismaModule,
+    MailModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET_KEY,
       signOptions: { expiresIn: '15m' },
-    })
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
