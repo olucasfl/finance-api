f8ba61aa7c1e727ae75a6ea1e0309725903ab96d
Lucas Leandro <lucasfariasleandro@gmail.com>
2026-03-11 17:53:34 -0300

    fix: CORS voxai

--- src/main.ts
+++ src/main.ts
@@ -7,20 +7,36 @@ async function bootstrap() {
  const app = await NestFactory.create(AppModule)
 
  app.enableCors({
-  origin:[
+  origin: [
    "http://localhost:5173",
    "https://oratio.vercel.app"
   ],
-  methods:["GET","POST","PUT","DELETE","OPTIONS"],
-  allowedHeaders:["Content-Type","Authorization"],
-  credentials:true
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"],
+  credentials: true
+ })
+
+ /* IMPORTANTE PARA VERCEL */
+
+ app.use((req, res, next) => {
+
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+  if (req.method === "OPTIONS") {
+   return res.sendStatus(200)
+  }
+
+  next()
+
  })
 
  app.useGlobalPipes(
   new ValidationPipe({
-   whitelist:true,
-   forbidNonWhitelisted:true,
-   transform:true
+   whitelist: true,
+   forbidNonWhitelisted: true,
+   transform: true
   })
  )
 
