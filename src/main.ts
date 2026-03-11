42b5ae45f644ef6aaeb609879e19a4ae16b254a9
Lucas Leandro <lucasfariasleandro@gmail.com>
2026-03-11 17:48:45 -0300

    refactor: change main.ts to CORS

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,31 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: true,
-    credentials: true,
+ const app = await NestFactory.create(AppModule)
+
+ app.enableCors({
+  origin:[
+   "http://localhost:5173",
+   "https://oratio.vercel.app"
+  ],
+  methods:["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders:["Content-Type","Authorization"],
+  credentials:true
+ })
+
+ app.useGlobalPipes(
+  new ValidationPipe({
+   whitelist:true,
+   forbidNonWhitelisted:true,
+   transform:true
   })
+ )
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
+ await app.listen(process.env.PORT ?? 3000)
 
-  await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+bootstrap()
