eed8529a24f6620ccc815d85a46353649d02350f
Lucas Leandro <lucasfariasleandro@gmail.com>
2026-03-13 18:54:48 -0300

    commit consecration changes routes

---
+++ src/modules/oratio/consecration/consecration.controller.ts
@@ -0,0 +1,158 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Req,
+  UseGuards
+} from '@nestjs/common';
+
+import { ConsecrationService } from './consecration.service';
+import { JwtAuthGuard } from 'src/modules/auth/jwt-auth.guard';
+
+@Controller('oratio/consecration')
+export class ConsecrationController {
+
+  constructor(private readonly service: ConsecrationService) {}
+
+@Post('start')
+@UseGuards(JwtAuthGuard)
+start(
+  @Req() req: any,
+  @Body() body: { consecrationDate: string }
+) {
+
+  const consecrationDate = new Date(body.consecrationDate)
+
+  const startDate = new Date(consecrationDate)
+  startDate.setDate(startDate.getDate() - 33)
+
+  return this.service.start(
+    req.user.userId,
+    startDate
+  )
+
+}
+
+  @Get('progress')
+  @UseGuards(JwtAuthGuard)
+  progress(@Req() req: any) {
+
+    return this.service.progress(req.user.userId);
+
+  }
+
+  @Get('day/:day')
+  getDay(@Param('day') day: string) {
+
+    return this.service.findDay(Number(day));
+
+  }
+
+  @Post('stage')
+  createStage(@Body() body: any) {
+
+    return this.service.createStage(body);
+
+  }
+
+  @Post('day')
+  createDay(@Body() body: any) {
+
+    return this.service.createDay(body);
+
+  }
+
+  @Post('prayer')
+  createPrayer(@Body() body: any) {
+
+    return this.service.createPrayer(body);
+
+  }
+
+  @Post('day-prayer')
+  addPrayerToDay(@Body() body: any) {
+
+    return this.service.addPrayerToDay(body);
+
+  }
+
+  @Put('day-prayer/:id')
+  updateDayPrayer(
+    @Param('id') id: string,
+    @Body() body: { order: number }
+  ) {
+    return this.service.updateDayPrayer(id, body.order);
+  }
+
+  @Put('prayer/:id')
+  updatePrayer(
+    @Param('id') id: string,
+    @Body() body: { title?: string; content?: string }
+  ) {
+    return this.service.updatePrayer(id, body);
+  }
+
+  @Get()
+  getAll() {
+    return this.service.getFullConsecration();
+  }
+
+  @Get('today')
+  @UseGuards(JwtAuthGuard)
+  today(@Req() req: any) {
+    return this.service.today(req.user.userId);
+  }
+  
+  @Post('reset')
+  @UseGuards(JwtAuthGuard)
+  reset(@Req() req: any) {
+    return this.service.reset(req.user.userId);
+  }
+
+  @Post("complete/:day")
+  @UseGuards(JwtAuthGuard)
+  complete(@Req() req:any, @Param("day") day:string){
+    return this.service.completeDay(req.user.userId,Number(day))
+  }
+
+@Put("consecration-date")
+updateStartDate(
+ @Req() req:any,
+ @Body() body:{consecrationDate:string}
+){
+
+ const consecrationDate = new Date(body.consecrationDate)
+
+ const startDate = new Date(consecrationDate)
+ startDate.setDate(startDate.getDate() - 33)
+
+ return this.service.updateStartDate(
+  req.user.userId,
+  startDate
+ )
+
+}
+
+  @Get("stage/:stageId/days")
+  getStageDays(@Param("stageId") stageId: string) {
+
+    return this.service.getStageDays(stageId)
+
+  }
+
+  @Delete("complete/:day")
+  @UseGuards(JwtAuthGuard)
+  uncompleteDay(@Req() req:any, @Param("day") day:string){
+
+    return this.service.uncompleteDay(req.user.userId, Number(day))
+  }
+
+  @Get("/all-days")
+  getAllDays(){
+    return this.service.getAllDays()
+  }
+}
